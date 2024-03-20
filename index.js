0239323bb51f8e275215b53b6ff5f8f4ac28301f
Umuratwa <umuratwar@gmail.com>
2024-03-20 11:56:56 +0200

    my portfolio

---
+++ index.js
@@ -0,0 +1,9 @@
+//  <script>
+//     typing animation
+//     var typed = new Typed(".typing", {
+//         strings: ["Web Designer", "Web Developer", "Video Editor"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+// </script> 
